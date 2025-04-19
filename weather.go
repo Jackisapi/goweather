e89df15e4426b031bba9d00d2bd75bf92ae61fb9
Jack W <getphyk3d@gmail.com>
2025-04-18 23:27:54 -0600

    Got a nice formater

--- weather.go
+++ weather.go
@@ -6,10 +6,10 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strconv"
+	"time"
 )
 
-// sets how the data is to be contained
+// WeatherResponse sets how the data is to be contained
 type WeatherResponse struct {
 	Name     string `json:"name"`
 	Timezone int    `json:"timezone"`
@@ -28,24 +28,39 @@ type WeatherResponse struct {
 	} `json:"wind"`
 }
 
-// Sets the cords into a list
-func setCords(latitude float64, longitude float64) []string {
-	// checks to see if latitude is beyond range
-	if latitude > 90 || latitude < -90 {
-		fmt.Println("latitude must be between 0 and 90")
-		//exits if so
-		os.Exit(1)
-		//checks to see if longitude is beyond range
-	} else if longitude > 180 || longitude < -180 {
-		fmt.Println("longitude must be between 0 and 180")
-		//exits if so
-		os.Exit(1)
-	}
-	//converts the cords into a string and puts them into a list
-	cords := []string{strconv.FormatFloat(latitude, 'f', 2, 64), strconv.FormatFloat(longitude, 'f', 2, 64)}
-	// Finally returns the value
-	return cords
+// Assures that when the struct is printed it doest look trashy
+func (w WeatherResponse) String() string {
+	t := time.Unix(w.DT, 0).UTC()
+	return fmt.Sprintf("City: %s\nTime: %s\nTemp: %.1f°C\nFeels Like: %.1f°C\nHumidity: %d%% \nWind: (Speed: %f, Angle: %d) \n Desc: %s",
+		w.Name,
+		t.Format("2006-01-02 15:04:05"),
+		w.Main.Temp,
+		w.Main.FeelsLike,
+		w.Main.Humidity,
+		w.Wind.Speed,
+		w.Wind.Deg,
+		w.Weather[0].Description,
+	)
 }
+
+//// Sets the cords into a list
+//func setCords(latitude float64, longitude float64) []string {
+//	// checks to see if latitude is beyond range
+//	if latitude > 90 || latitude < -90 {
+//		fmt.Println("latitude must be between 0 and 90")
+//		//exits if so
+//		os.Exit(1)
+//		//checks to see if longitude is beyond range
+//	} else if longitude > 180 || longitude < -180 {
+//		fmt.Println("longitude must be between 0 and 180")
+//		//exits if so
+//		os.Exit(1)
+//	}
+//	//converts the cords into a string and puts them into a list
+//	cords := []string{strconv.FormatFloat(latitude, 'f', 2, 64), strconv.FormatFloat(longitude, 'f', 2, 64)}
+//	// Finally returns the value
+//	return cords
+//}
 
 //This does not work lol
 //func weatherReqLatLong(api string, cords []string) *http.Response {
@@ -62,7 +77,7 @@ func weatherReqCity(api string, city string) WeatherResponse {
 	url = fmt.Sprintf(url, city, api)
 	//makes the request
 	weather, _ := http.Get(url)
-	// waits until request pings back to continue running errors if it doesnt
+	// waits until request pings back to continue running errors if it doesn't
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
@@ -70,7 +85,7 @@ func weatherReqCity(api string, city string) WeatherResponse {
 		}
 	}(weather.Body)
 
-	//creates json template using the scruct built above
+	//creates json template using the struct built above
 	var result WeatherResponse
 	//encodes the response into the above format and returns
 	data := json.NewDecoder(weather.Body).Decode(&result)
@@ -82,10 +97,7 @@ func weatherReqCity(api string, city string) WeatherResponse {
 
 func main() {
 	var key = os.Getenv("WEATHER_KEY")
-	fmt.Println("WEATHER_KEY:", key)
-	test := setCords(10, 20)
-	fmt.Println(test)
 	//fmt.Println(weatherReqLatLong(key, test))
-	boise_weather := weatherReqCity(key, "Boise")
-	fmt.Println(boise_weather)
+	boiseWeather := weatherReqCity(key, "Boise")
+	fmt.Println(boiseWeather)
 }
