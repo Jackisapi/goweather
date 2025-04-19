5d4e1f80fe735b5043fa7dd527aa4b5fead63f63
Jack W <getphyk3d@gmail.com>
2025-04-18 19:25:43 -0600

    Initial commit

--- weather.go
+++ weather.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 )
 
+// sets how the data is to be contained
 type WeatherResponse struct {
 	Name string `json:"name"`
 	Main struct {
@@ -20,18 +21,26 @@ type WeatherResponse struct {
 	} `json:"weather"`
 }
 
+// Sets the cords into a list
 func setCords(latitude float64, longitude float64) []string {
+	// checks to see if latitude is beyond range
 	if latitude > 90 || latitude < -90 {
 		fmt.Println("latitude must be between 0 and 90")
+		//exits if so
 		os.Exit(1)
+		//checks to see if longitude is beyond range
 	} else if longitude > 180 || longitude < -180 {
 		fmt.Println("longitude must be between 0 and 180")
+		//exits if so
 		os.Exit(1)
 	}
+	//converts the cords into a string and puts them into a list
 	cords := []string{strconv.FormatFloat(latitude, 'f', 2, 64), strconv.FormatFloat(longitude, 'f', 2, 64)}
+	// Finally returns the value
 	return cords
 }
 
+//This does not work lol
 //func weatherReqLatLong(api string, cords []string) *http.Response {
 //	url := "https://api.openweathermap.org/data/3.0/onecall?lat=%s&lon=%s&appid=%s"
 //	url = fmt.Sprintf(url, cords[0], cords[1], api)
@@ -42,8 +51,11 @@ func setCords(latitude float64, longitude float64) []string {
 
 func weatherReqCity(api string, city string) WeatherResponse {
 	url := "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=imperial"
+	//formats the request url with the city, and api key
 	url = fmt.Sprintf(url, city, api)
+	//makes the request
 	weather, _ := http.Get(url)
+	// waits until request pings back to continue running errors if it doesnt
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
@@ -51,7 +63,9 @@ func weatherReqCity(api string, city string) WeatherResponse {
 		}
 	}(weather.Body)
 
+	//creates json template using the scruct built above
 	var result WeatherResponse
+	//encodes the response into the above format and returns
 	err := json.NewDecoder(weather.Body).Decode(&result)
 	if err != nil {
 		return WeatherResponse{}
